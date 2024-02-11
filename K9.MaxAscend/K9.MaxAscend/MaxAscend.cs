541494a66306abde712fae2a6c9229e010ace4ce
ChanBormey10Mao <102600004@student.swin.edu.au>
2024-02-11 18:41:39 +1100

    feat: Find Biggest Sorted Sublist func and main

--- K9.MaxAscend/K9.MaxAscend/MaxAscend.cs
+++ K9.MaxAscend/K9.MaxAscend/MaxAscend.cs
@@ -8,5 +8,33 @@ namespace K9.MaxAscend
 {
     public class MaxAscend
     {
+        public static string Main(string input)
+        {
+            int[] inputNumbers = Array.ConvertAll(input.Split(' '), int.Parse);
+            return string.Join(" ", FindBiggestSortedSublist(inputNumbers));
+
+        }
+        public static List<int> FindBiggestSortedSublist(int[] numList)
+        {
+            List<int> potentialResultListCurrent = new List<int>();
+            List<int> potentialResultListBiggest = new List<int>();
+            for (int i = 0; i < numList.Count() - 1; i++)
+            {
+                if (numList[i] < numList[i + 1])
+                {
+                    potentialResultListCurrent.Add(numList[i]);
+                }
+                else
+                {
+                    potentialResultListCurrent.Add(numList[i]);
+                    if (potentialResultListCurrent.Count > potentialResultListBiggest.Count)
+                    {
+                        potentialResultListBiggest = potentialResultListCurrent;
+                    }
+                    potentialResultListCurrent = new List<int>();
+                }
+            }
+            return potentialResultListBiggest;
+        }
     }
 }
