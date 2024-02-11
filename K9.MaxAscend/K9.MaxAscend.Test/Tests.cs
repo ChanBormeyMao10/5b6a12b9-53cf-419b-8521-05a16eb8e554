723146f53af889946f7da54f607e7892a73728c8
ChanBormey10Mao <102600004@student.swin.edu.au>
2024-02-11 18:43:23 +1100

    feat: Find Biggest Sorted Sublist - Unit Test

---
+++ K9.MaxAscend/K9.MaxAscend.Test/Tests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K9.MaxAscend.Test
+{
+    [TestFixture]
+    public class Tests
+    {
+        /* Test case 1 */
+        [Test]
+        public string TestFindBiggestSortedSublist(string input)
+        {
+            return MaxAscend.Main(input);
+        }
+    }
+}
